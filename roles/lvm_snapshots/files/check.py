e3e98c9c41ac070176a192ffd3d03ed83b71afdf
Ygal Blum <ygal.blum@gmail.com>
2023-07-24 18:20:58 +0300

    Add support for checking before resizing volumes (#2)
    * Add support for checking before resizing volumes
    
    Adjust the check script to also support resize checks
    Add tasks for check resize and use them according to the command
    Add test playbook
    Add change log
    
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>
    
    * Find the python executable using ansible facts
    
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>
    
    * Do not raise from exception to support python2
    
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>
    
    * fix typo
    
    ---------
    
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>
    Co-authored-by: Bob Mader <bmader@redhat.com>

--- roles/lvm_snapshots/files/check.py
+++ roles/lvm_snapshots/files/check.py
@@ -8,24 +8,40 @@ Each element should have the following keys:
         Follow (https://docs.ansible.com/ansible/latest/collections/community/general/lvol_module.html#parameter-size)
         without support for sign
 '''
+import argparse
 import json
+import math
+import os
 import subprocess
 import sys
 
 _VGS_COMMAND = '/usr/sbin/vgs'
 _LVS_COMMAND = '/usr/sbin/lvs'
 
+_EXIT_CODE_SUCCESS = 0
+_EXIT_CODE_VOLUME_GROUP_SPACE = 1
+_EXIT_CODE_FILE_SYSTEM_TYPE = 2
+_EXIT_CODE_VOLUME_SPACE = 3
+
+_supported_filesystems = [
+    '',
+    'ext2',
+    'ext3',
+    'ext4'
+]
+
 class CheckException(Exception):
     """ Exception wrapper """
 
+parser = argparse.ArgumentParser()
+parser.add_argument('that', help='What should the script check', type=str, choices=['snapshots', 'resize'])
+parser.add_argument('volumes', help='Volumes JSON array in a string', type=str)
 
 def _main():
-    if len(sys.argv) < 2 or not sys.argv[1]:
-        print("List of volumes was not provided")
-        sys.exit(1)
+    args = parser.parse_args()
 
     try:
-        volumes = json.loads(sys.argv[1])
+        volumes = json.loads(args.volumes)
     except json.decoder.JSONDecodeError:
         print("Provided volume list '{volumes}' it not a valid json string".format(volumes=sys.argv[1]))
         sys.exit(1)
@@ -36,14 +52,53 @@ def _main():
     }
 
     for vol in volumes:
-        groups_info[vol["vg"]]['requested_size'] += _calc_requested_size(groups_info[vol["vg"]], vol)
+        vol['normalized_size'] = _calc_requested_size(groups_info[vol["vg"]], vol)
+        groups_info[vol["vg"]]['requested_size'] += vol['normalized_size']
 
-    enough_space = all(group['requested_size'] > group['free'] for _, group in groups_info.items())
+    if args.that == 'snapshots':
+        exit_code = _check_free_size_for_snapshots(groups_info)
+    if args.that == 'resize':
+        exit_code = _check_free_size_for_resize(volumes, groups_info)
 
+    sys.exit(exit_code)
+
+
+def _check_free_size_for_snapshots(groups_info):
+    return _check_requested_size(groups_info, 'free')
+
+
+def _check_free_size_for_resize(volumes, groups_info):
+    exit_code = _check_requested_size(groups_info, 'size')
+    if exit_code != _EXIT_CODE_SUCCESS:
+        return exit_code
+
+    mtab = _parse_mtab()
+
+    for volume in volumes:
+        mtab_entry = mtab.get("/dev/mapper/{vg}-{lv}".format(vg=volume['vg'], lv=volume['lv']))
+        volume['fs_type'] = mtab_entry['type'] if mtab_entry else ''
+        volume['fs_size'] = _calc_filesystem_size(mtab_entry) if mtab_entry else 0
+
+    filesystems_supported = all(volume['fs_type'] in _supported_filesystems for volume in volumes)
+    if not filesystems_supported:
+        exit_code = _EXIT_CODE_FILE_SYSTEM_TYPE
+
+    enough_space = all(vol['normalized_size'] > vol['fs_size'] for vol in volumes)
     if not enough_space:
-        sys.exit(0)
-    print(json.dumps(groups_info))
-    sys.exit(1)
+        exit_code = _EXIT_CODE_VOLUME_SPACE
+
+    if exit_code != _EXIT_CODE_SUCCESS:
+        print(json.dumps(_to_printable_volumes(volumes)))
+
+    return exit_code
+
+
+def _check_requested_size(groups_info, group_field):
+    enough_space = all(group['requested_size'] <= group[group_field] for _, group in groups_info.items())
+    if not enough_space:
+        print(json.dumps(groups_info))
+        return _EXIT_CODE_VOLUME_GROUP_SPACE
+    return _EXIT_CODE_SUCCESS
 
 
 def _get_group_info(group):
@@ -84,8 +139,8 @@ def _calc_requested_size(group_info, volume):
             try:
                 size = int(requested_size[:-1])
                 unit = requested_size[-1].lower()
-            except ValueError as exc:
-                raise CheckException('Failed to read requested size {size}'.format(size=requested_size)) from exc
+            except ValueError:
+                raise CheckException('Failed to read requested size {size}'.format(size=requested_size))
     return _convert_to_bytes(size, unit)
 
 
@@ -110,6 +165,23 @@ def _get_size_from_report(reported_size):
     return _convert_to_bytes(size, unit)
 
 
+def _calc_filesystem_size(mtab_entry):
+    fs_stat = os.statvfs(mtab_entry['mount_point'])
+    return (fs_stat.f_blocks - fs_stat.f_bfree) * fs_stat.f_bsize
+
+
+def _parse_mtab():
+    mtab = {}
+    with open('/etc/mtab') as f:
+        for m in f:
+            fs_spec, fs_file, fs_vfstype, _fs_mntops, _fs_freq, _fs_passno = m.split()
+            mtab[fs_spec] = {
+                'mount_point': fs_file,
+                'type': fs_vfstype
+            }
+    return mtab
+
+
 def _convert_to_bytes(size, unit):
     convertion_table = {
         'b': 1024 ** 0,
@@ -122,6 +194,26 @@ def _convert_to_bytes(size, unit):
     }
     return size * convertion_table[unit]
 
+
+def _convert_to_unit_size(bytes):
+    units = ['b', 'k', 'm', 'g', 't', 'p', 'e']
+    i = 0
+    while bytes >= 1024:
+        i += 1
+        bytes /= 1024
+    # Round down bytes to two digits
+    bytes = math.floor(bytes * 100) / 100
+    return "{size}{unit}".format(size=bytes, unit=units[i])
+
+
+def _to_printable_volumes(volumes):
+    return {
+        volume['vg'] + "_" + volume['lv']: {
+            'file_system_type': volume['fs_type'],
+            'used': _convert_to_unit_size(volume['fs_size']),
+            'requested_size': _convert_to_unit_size(volume['normalized_size'])
+        } for volume in volumes
+    }
 
 if __name__ == '__main__':
     _main()
