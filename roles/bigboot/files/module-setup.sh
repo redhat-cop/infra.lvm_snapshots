fb014b65d78d9e0c94a32366f6178b282b62deef
Ygal Blum <ygal.blum@gmail.com>
2023-11-30 14:52:04 +0200

    Shellcheck - fix errors and add to CI (#26)
    Fix shellcheck errors in the existing code
    Add shellcheck workflow to GitHub CI
    Add the shellcheck workflow to the dependencies of the release workflow

--- roles/bigboot/files/module-setup.sh
+++ roles/bigboot/files/module-setup.sh
@@ -8,6 +8,7 @@ check(){
 
 install() {
     inst_multiple -o /usr/bin/mount /usr/bin/umount /usr/sbin/parted /usr/bin/mktemp /usr/bin/wc /usr/bin/date /usr/bin/sed /usr/bin/awk /usr/bin/basename /usr/sbin/resize2fs /usr/sbin/tune2fs /usr/sbin/partprobe /usr/bin/numfmt /usr/sbin/lvm /usr/bin/lsblk /usr/sbin/e2fsck /usr/sbin/fdisk /usr/bin/findmnt /usr/bin/tail /usr/ /usr/sbin/xfs_growfs /usr/sbin/xfs_db
+    # shellcheck disable=SC2154
     inst_hook pre-mount 99 "$moddir/increase-boot-partition.sh"
     inst_binary "$moddir/sfdisk.static" "/usr/sbin/sfdisk"
     inst_simple "$moddir/bigboot.sh" "/usr/bin/bigboot.sh"
