c20329f1d5dd3d9e14ce6f39f4df2b3479af9993
Bob Mader <bmader@redhat.com>
2024-04-25 07:49:16 -0500

    don't use lvm.conf to disable locking (#65)
    * don't use lvm.conf to disable locking
    
    add /usr/bin/sort
    
    use nolocking options
    
    changelog
    
    fix shellcheck findings
    
    add /usr/libexec/lvresize_fs_helper
    
    add lvresize_fs_helper dependencies
    
    * use full path

--- roles/shrink_lv/files/module-setup.sh
+++ roles/shrink_lv/files/module-setup.sh
@@ -7,7 +7,7 @@ check(){
 }
 
 install() {
-    inst_multiple -o /usr/bin/numfmt /usr/bin/findmnt /usr/bin/lsblk /usr/sbin/lvm /usr/bin/awk /usr/bin/sed /usr/bin/mktemp /usr/bin/date /usr/bin/head /usr/sbin/blockdev /usr/sbin/tune2fs /usr/sbin/resize2fs /usr/bin/cut /usr/sbin/fsadm /usr/sbin/fsck.ext4
+    inst_multiple -o /usr/bin/numfmt /usr/bin/findmnt /usr/bin/lsblk /usr/sbin/lvm /usr/bin/awk /usr/bin/sed /usr/bin/sort /usr/bin/mktemp /usr/bin/date /usr/bin/head /usr/sbin/blockdev /usr/sbin/tune2fs /usr/sbin/resize2fs /usr/bin/cut /usr/sbin/fsadm /usr/sbin/fsck.ext4 /usr/libexec/lvresize_fs_helper /usr/sbin/cryptsetup /usr/bin/logger /usr/bin/basename /usr/bin/getopt
     # shellcheck disable=SC2154
     inst_hook pre-mount 99 "$moddir/shrink-start.sh"
     inst_simple "$moddir/shrink.sh" "/usr/bin/shrink.sh"
